e74cbdb6e0383af2dfdf54f79db12b4d3808a0f8
Scibi S V <scibi.s.v@gmail.com>
2020-12-18 11:06:46 +0530

    my code

---
+++ app/src/main/java/com/example/sqlitetest/GroceryDBHelper.java
@@ -0,0 +1,39 @@
+package com.example.sqlitetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class GroceryDBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="grocerylist.db";
+    public static final int DATABASE_VERSION=1;
+
+    public GroceryDBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME,  null, 1);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_GROCERYLIST_TABLE = "CREATE TABLE " +
+                GroceryContract.GroceryEntry.TABLE_NAME + " (" +
+                GroceryContract.GroceryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                GroceryContract.GroceryEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                GroceryContract.GroceryEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " +
+                GroceryContract.GroceryEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+        db.execSQL(SQL_CREATE_GROCERYLIST_TABLE);
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + GroceryContract.GroceryEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
